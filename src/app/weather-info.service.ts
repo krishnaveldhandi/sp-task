2da4a0a9a66fb6b6df445139b040102b6a8c5bd8
sai krishna v <saikrishnav@sumpositive.com>
2024-04-08 21:07:10 +0530

    Removed Weather Api URL

--- src/app/weather-info.service.ts
+++ src/app/weather-info.service.ts
@@ -6,10 +6,4 @@ import { Injectable } from '@angular/core';
 })
 export class WeatherInfoService {
   constructor(private http: HttpClient) {}
-
-  getUrl() {
-    return this.http.get(
-      'https://api.openweathermap.org/data/2.5/weather?q=New%20York%20&appid=b646987be1518a2a1730f4e71113de11&units=imperial'
-    );
-  }
 }
